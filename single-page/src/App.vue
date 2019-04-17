af5ff9b6a0cdb7198cca3c5fc866aa16f127f1e3
luzhanzhao <a390743684@live.com>
2019-04-18 00:46:34 +0800

    实现路由权限控制 实现菜单权限控制

--- single-page/src/App.vue
+++ single-page/src/App.vue
@@ -4,26 +4,6 @@
   </div>
 </template>
 <script>
-  import axios from 'axios'
-  import {getRoutes} from './router/routerMap'
-  export default{
-    mounted(){
-      let self = this;
-      if (localStorage.getItem("role")){
-        axios.get('/json/Shiro.json').then(res => {// 拉取用户信息
-          const roles = res.data
-          let rolesKey = Object.keys(roles);
-        for(var i = 0; i < rolesKey.length; i++){
-          if(rolesKey[i] == self.$store.state.role){
-            let routes = getRoutes(roles[rolesKey[i]]);
-            self.$store.commit("setPowerMenu",routes);
-            self.$router.addRoutes(routes);
-          }
-        }
-      })
-      }
-    }
-  }
 </script>
 <style lang="scss">
 #app {
